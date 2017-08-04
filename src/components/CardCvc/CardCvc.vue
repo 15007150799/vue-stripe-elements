a4c275e9715dd7269fcc180e51c69815ab0cd295
Stan Guo <shitaoguo@gmail.com>
2017-08-03 23:38:38 -0700

    feat: add card, postal code components

--- src/components/CardCvc/CardCvc.vue
+++ src/components/CardCvc/CardCvc.vue
@@ -1,5 +1,5 @@
 <template>
-  <div></div>
+  <div/>
 </template>
 
 <script>
@@ -16,5 +16,3 @@ export default {
   }
 }
 </script>
-
-<style></style>
---
+++ src/components/PostalCode/index.js
@@ -0,0 +1,8 @@
+import PostalCode from './PostalCode.vue'
+
+/* istanbul ignore next */
+PostalCode.install = function(Vue) {
+  Vue.component(PostalCode.name, PostalCode)
+}
+
+export default PostalCode
